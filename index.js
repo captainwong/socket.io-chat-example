13d5259a92bb384f4621991236b86dba502eaf84
hb168 <1281261856@qq.com>
2021-08-14 04:06:07 +0800

    fix

--- index.js
+++ index.js
@@ -19,16 +19,18 @@ io.on('connection', (socket) => {
     let addedUser = false;
 
     socket.on('new message', (data) => {
-        console.log('on new message:');
-        socket.broadcast.emit('new message', {
-            username: socket.username,
-            message: data
-        });
+        if (addedUser) {
+            console.log('on new message:');
+            socket.broadcast.emit('new message', {
+                username: socket.username,
+                message: data
+            });
+        }
     });
 
     socket.on('add user', (username) => {
         console.log('on add user:', username);
-        if(addedUser) return;
+        if (addedUser) return;
 
         socket.username = username;
         ++numUsers;
@@ -46,21 +48,25 @@ io.on('connection', (socket) => {
 
     socket.on('typing', () => {
         console.log('on typing:', socket.username);
-        socket.broadcast.emit('typing', {
-            username: socket.username
-        });
+        if (addedUser) {
+            socket.broadcast.emit('typing', {
+                username: socket.username
+            });
+        }
     });
 
     socket.on('stop typing', () => {
         console.log('on stop typing:', socket.username);
-        socket.broadcast.emit('stop typing', {
-            username: socket.username
-        });
+        if (addedUser) {
+            socket.broadcast.emit('stop typing', {
+                username: socket.username
+            });
+        }
     });
 
     socket.on('disconnect', () => {
         console.log('on disconnect:', socket.username);
-        if(addedUser){
+        if (addedUser) {
             --numUsers;
             socket.broadcast.emit('user left', {
                 username: socket.username,
